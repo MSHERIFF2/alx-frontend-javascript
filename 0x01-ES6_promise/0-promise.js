7a0316a87aeb737ddffdf1410af8be6b9f62a4af
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-11-02 18:54:44 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -2,7 +2,7 @@ export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const response = {data: "Hello From API"}
-      resolve(response)
+        resolve(response)
     }, 2000)
   });
 }
