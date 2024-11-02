2dcc825004d624826a27991365eb767b3f06a3fa
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-11-02 18:39:45 +0100

    Update 0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -10,10 +10,10 @@ function getResponseFromAPI() {
   });
 }
 
-getResponseFromAPI()
-  .then(response => {
-    console.log(response);
-  })
-  .catch(error => {
-    console.error(error);
-  });
+// getResponseFromAPI()
+//   .then(response => {
+//     console.log(response);
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
