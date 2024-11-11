38395c80156c3c6ce2424e4f2a3445bc9b123b68
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-11-11 11:55:15 +0000

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,6 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    promise
-      .then(() => {
-        resolve({
-          status: 200,
-          body: 'success',
-        });
-      })
-      .catch(() => {
-        const errors = new Error('');
-        reject(errors);
-      });
-    console.log('Got a response from the API');
-  });
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
